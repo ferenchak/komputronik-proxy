8d6c68b8bf19fa6074979df7e884f4199cafd755
ferenchak <101417272+ferenchak@users.noreply.github.com>
2025-05-30 17:58:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,5 +44,6 @@ def parse_product():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    app.run(debug=True, host="0.0.0.0", port=10000)
+
 
